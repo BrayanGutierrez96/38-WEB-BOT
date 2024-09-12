71e569a9edab838c2d68550fc296f62143079b34
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2024-09-12 16:32:00 -0500

    Se agrega la carpeta del modulo 2 dia 1

---
+++ M2 - INTRO A JAVASCRIPT/DIA 1 - JAVASCRIPT/index.js
@@ -0,0 +1,39 @@
+// Tipos de variable
+//de tipo VAR su alcance es global
+// var variable1 = true;
+// var variable2 = 1;
+// var variable3 = "una cadena"
+// var variable4 = null
+// var variable5 = { llave: "valor", llave2 : [] }
+// var variable6 = [{llave: "valor", llave2 : []}, 1 ,"hola", [], undefined]
+// var variable7 = undefined
+
+// console.log("variable5: " + variable5);
+
+
+// de tipo Let su alcance es mas limitado
+let userName = false
+let variable2 = 2
+let variable3 = "dos cadenas"
+let variable4 = null
+let variable5 = [{llave: "valor", llave2 : []}, 1 ,"hola", [], undefined]
+let variable6 = { llave: "valor", llave2 : [] }
+let variable7 = undefined
+
+variable5 = false
+
+
+// console.log(variable2);
+// console.log(variable5);
+
+//de tipo constante No cambia su valor despues de definida
+const variableConstante = 10
+
+
+
+console.log(variable3 + " " + (variableConstante * variable2));
+
+"dos cadenas 102" 
+
+
+
