5573bf286f07331163ee7779ef97205eb760537b
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2024-11-26 19:19:44 -0500

    Se agrega el ejercicio explicativo sobre promesas

--- M4 - JAVASCRIPT AVANZADO/DIA 3 - PETICIONES/src/main.js
+++ M4 - JAVASCRIPT AVANZADO/DIA 3 - PETICIONES/src/main.js
@@ -10,3 +10,35 @@ console.log("hola mundo5");
 const day = timeDay();
 
 console.log(day);
+
+// then catch
+fetch("https://fakestoreapi.com/products")
+  .then(function (productos) {
+    return productos.json();
+  })
+  .then(function (productos) {
+    const productosSimples = productos.map(function (producto) {
+      return {
+        titulo: producto.title,
+        id: producto.id,
+        precio: producto.price,
+      };
+    });
+    console.log(productosSimples);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+//async/await
+
+async function obtenerProductos() {
+  try {
+    const productos = await fetch("https://fakestoreapi.com/products");
+    const productosParseados = await productos.json();
+    console.log(productosParseados);
+  } catch (error) {
+    console.log(error);
+  }
+}
+obtenerProductos();
