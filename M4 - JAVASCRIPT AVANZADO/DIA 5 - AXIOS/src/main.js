6d7257408c238c503261184713aae718b939db3d
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2024-12-03 19:14:16 -0500

    Se agrega los ejemplos de fetch y axios

--- M4 - JAVASCRIPT AVANZADO/DIA 5 - AXIOS/src/main.js
+++ M4 - JAVASCRIPT AVANZADO/DIA 5 - AXIOS/src/main.js
@@ -3,19 +3,37 @@ const URL = "https://example-unicorns-api.vercel.app/unicornios";
 
 //#1 METODO GET ---> OBTENER DATOS DE UNA API
 
-fetch(URL)
-  .then((res) => {
-    return res.json();
-  })
-  .then((res) => {
-    console.log(res);
-  }); //--> FETCH INICIA POR DEFECTO EN METODO GET "this"
+// fetch(URL)
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   }); //--> FETCH INICIA POR DEFECTO EN METODO GET "this"
 
 // #2 METODO POST ---> CREAR DATOS EN UNA API
-const data = { name: "SKY3", power: "FLY", img: "z" }
-fetch(URL, {
-  method: "POST",
-  headers:{  "Content-Type": "application/json"},
-  body: JSON.stringify(data),
-}).then((res)=>{return res});
+// const data = { name: "SKY3", power: "FLY", img: "z" }
+// fetch(URL, {
+//   method: "POST",
+//   headers:{  "Content-Type": "application/json"},
+//   body: JSON.stringify(data),
+// }).then((res)=>{return res});
 
+//AXIOS ---> ME PERMITE HACER PETICIONES CON JS PERO ES UNA LIBRERIA
+import axios from "axios"; 
+//#1 METODO GET ---> OBTENER DATOS DE UNA API
+async function obtenerUnicornios() {
+   const respuesta =  await axios.get(URL);
+   console.log(respuesta.data);
+}
+
+
+//#2 METODO POST ---> CREAR DATOS EN UNA API
+
+async function crearUnicornio() {
+    const informacion = { name: "SUN", power: "FLY", img: "Y" }
+    await axios.post(URL, informacion)
+}
+
+// crearUnicornio()
+obtenerUnicornios()
